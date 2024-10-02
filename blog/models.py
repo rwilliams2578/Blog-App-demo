936e2b803a40f2c187a2e4427d90cdd3fff280d7
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-10-01 20:26:54 -0400

    create blog/migrations, blog/admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    """Blog Post model"""
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    body = models.TextField()
+
+    # These 2 dates are special in that they can't be set
+    # by a user. They are auto-srt by the system during
+    # creation and updating.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String method"""
+        return self.title
+
+    def get_absolute_url(self):
+        """Get absolute url for this instance"""
+        return reverse("post_detail", kwargs={"pk": self.pk})
