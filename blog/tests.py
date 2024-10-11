c6e68570c7cd1b081f48db62b003119958a063f8
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-10-11 16:44:19 -0500

    Add Create, Update, and Delete functionality for a post. Add tests to verify functionality.

--- blog/tests.py
+++ blog/tests.py
@@ -56,3 +56,36 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):
+        """Test post createview"""
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last(), title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_updateview(self):
+        """Test post update view"""
+        response = self.client.post(
+            reverse("post_edit", args="1")
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteview(self):
+        """Test post delete view"""
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 
@@ -18,8 +20,33 @@ class BlogDetailView(DetailView):
 
 
 class BlogCreateView(CreateView):
-    """Blog Post Create View"""
+    """Blog Create View"""
 
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+    def get_initial(self):
+        """Get initial form data"""
+        # Get the result of calling the parent method
+        initial_data = super().get_initial()
+        # Add the author so it is the resquests user
+        initial_data["author"] = self.request.user
+        # return the data.
+        return initial_data
+
+
+class BlogUpdateView(UpdateView):
+    """Blog Update View"""
+
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    """Blog Delete View"""
+
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
